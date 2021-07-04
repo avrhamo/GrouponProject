beba947e0c88c750cc80eba9698cbcc14d68a714
AA <avrhamo@gmail.com>
2021-07-04 22:45:21 +0300

    From my laptop

--- src/main/java/com/jb/test/Test.java
+++ src/main/java/com/jb/test/Test.java
@@ -7,14 +7,14 @@ import com.jb.exception.CustomCouponSystemException;
 import com.jb.facade.AdminFacade;
 import com.jb.facade.ClientFacade;
 
-import java.awt.*;
 import java.sql.SQLException;
 
 public class Test {
 
     public static final String ANSI_BLUE = "\u001B[34m";
     public static final String ANSI_RESET = "\u001B[0m";
-
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    private final static int testCase = 1;
     public static void main(String[] args) throws SQLException, CustomCouponSystemException, InterruptedException {
 
 
@@ -31,49 +31,56 @@ public class Test {
         //AdminFacade adminFacade = (AdminFacade) clientFacade;
         AdminFacade adminFacade = (AdminFacade) Clients.getInstance().login("admin@admin.com", "admin",1);
 
-        printTestTitle("Testing admin facade getAllCustomers()");
+        printTestTitle("Testing admin facade getAllCustomers()", 1);
         adminFacade.getAllCustomers().forEach(System.out::println);
 
-        printTestTitle("Testing admin facade addCustomer(customer) - Customer name Shimi");
+        printTestTitle("Testing admin facade addCustomer(customer) - Customer name Shimi", 1);
         Customer customer  = new Customer("Shimi", "Shalom", "shimi@gmail.com", "1234");
         adminFacade.addCustomer(customer);
 
-        printTestTitle("Testing admin facade getAllCustomers()");
+        printTestTitle("Testing admin facade getAllCustomers()", 1);
         adminFacade.getAllCustomers().forEach(System.out::println);
 
-        printTestTitle("Testing exception - admin facade addCustomer(customer) - Customer name Shimi");
+        printTestTitle("Testing exception - admin facade addCustomer(customer) - Customer name Shimi", 1);
         adminFacade.addCustomer(customer);
 
-        printTestTitle("Testing Admin facade getAllCompanies()");
+        printTestTitle("Testing Admin facade getAllCompanies()", 1);
         adminFacade.getAllCompanies().forEach(System.out::println);
 
-        printTestTitle("Testing Admin facade addCompany(TestCompany)");
+        printTestTitle("Testing Admin facade addCompany(TestCompany)", 1);
         Company TestCompany = new Company(7,"tetsCompany", "testCompany@Email.com", "TEST", null);
         adminFacade.addCompany(TestCompany);
 
-        printTestTitle("testing Admin facade getAllCompanies()");
+        printTestTitle("testing Admin facade getAllCompanies()", 1);
         adminFacade.getAllCompanies().forEach(System.out::println);
 
-        printTestTitle("Testing exception - Admin facade addCompany(TestCompany)");
+        printTestTitle("Testing exception - Admin facade addCompany(TestCompany)", 1);
         adminFacade.addCompany(TestCompany);
 
-        printTestTitle("Testing Admin facade updateCompany(TestCompany)");
-        TestCompany.setEmail("newTestEmail@newTestCompanyMail.com");
+        printTestTitle("Testing Admin facade updateCompany(TestCompany)", 1);
+        TestCompany.setEmail("newTestEmail1@newTestCompanyMail.com");
         adminFacade.updateCompany(TestCompany);
 
-        printTestTitle("Testing Admin facade getOneCompany(7)");
+        printTestTitle("Testing Admin facade getOneCompany(7)", 1);
         System.out.println(adminFacade.getOneCompany(TestCompany.getId()).toString());
 
-        printTestTitle("Testing Admin facade updateCompany(TestCompany) Trying to update company_id");
+        printTestTitle("Testing Admin facade updateCompany(TestCompany) Trying to update company_id", 1);
         TestCompany.setId(8);
         adminFacade.updateCompany(TestCompany);
 
-        printTestTitle("Testing Admin facade deleteCompany(6)");
+        printTestTitle("Testing Admin facade deleteCompany(6)", 1);
         adminFacade.deleteCompany(6);
 
+
+
     }
 
-    private static void printTestTitle(String testTitle) {
-        System.out.println(ANSI_BLUE + testTitle + ANSI_RESET);
+    private static void printTestTitle(String testTitle, int testType) {
+        if(testType == testCase){
+            System.out.println(ANSI_BLUE + testTitle + ANSI_RESET);
+        }else {
+            System.out.println(ANSI_YELLOW + testTitle + ANSI_RESET);
+        }
     }
+
 }
